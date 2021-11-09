5fac13043f833a4976c9e1368f2edb084640a975
Nick Marmann <nickmarmann14@gmail.com>
2021-11-09 12:22:58 -0500

    changed some stuff in script

--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
+const {whosThatPokemon, getPokemon} = require('./whosThatPokemon')
 
-let answer :string;
-let guesses :number = 0;
+let answer: string;
+let guesses: number = 0;
 let correct: number = 0;
 let guessStage:boolean = false; 
 
@@ -116,41 +117,3 @@ document.onkeydown = function (event) {
     }
 }
 
-async function whosThatPokemon(): Promise<string> {
-
-    const id: string = String(Math.floor(Math.random() * 898 + 1));
-
-
-    let retData: string = await (getPokemon(id));
-    let parsedData = JSON.parse(retData);
-
-    let pic :HTMLImageElement= (document.getElementById("whosthatpokemon") as HTMLImageElement);
-    pic.src = parsedData.sprites.front_default;
-    pic.style.filter = "brightness(0%)"; 
-    pic.height = 200;
-    pic.width = 200;
-
-    return parsedData.name;
-}
-
-async function getPokemon(id: String): Promise<string> {
-    const http = new XMLHttpRequest();
-    const url: string = "https://pokeapi.co/api/v2/pokemon/" + id;
-
-    let ret;
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            ret = http.responseText;
-        }
-        else {
-            ret = undefined;
-        }
-    }
-    http.open("GET", url, false);
-    http.send();
-
-
-
-    return ret;
-
-}
